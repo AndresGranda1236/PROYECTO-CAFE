4ac8940ca27e710ecafe411dc0b682db98edbbde
Andres <andresfelipegrandadiaz@gmail.com>
2025-08-13 21:08:02 -0500

    VALITA

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,5 +135,5 @@ document.addEventListener("DOMContentLoaded", () => {
   // Cierra automáticamente después de 6 segundos
   setTimeout(() => {
     banner.style.display = "none";
-  }, 6000);
+  }, 2000);
 });
